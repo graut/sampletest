e0afd5bfc60b73ed419b495c5c6e873433956e96
graut <ghanshyam.agile@gmail.com>
2016-09-09 12:49:12 +0530

    AndroidAPIcreate
    AndroidAPIcreate

--- test.js
+++ test.js
@@ -1,45 +1,95 @@
-/**
- * Created by phelipemaia on 2/7/16.
- */
-var AgileCRMManager = require("./agilecrm.js");
-var obj = new AgileCRMManager("ghanshyam", "123456", "ghanshyam.raut@agilecrm.com");
-var success = function (data) {
-    console.log(data);
-	console.log("hello");
-};
-var error = function (data) {
-    console.log(data);
-	console.log("Hii");
-};
- //obj.contactAPI.getListContacts(success, error);
+private class RestOperation extends AsyncTask< String, Void, Void> {
 
-//obj.contactAPI.getContactByEmail('support+id18200@agilecrm.zendesk.com', success, error);
+        String content;
+        String error;
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        String data = "";
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.i(TAG,"Please Wait1 .......................");
+        }
 
-//obj.contactAPI.getContactById('5736096881180672', success, error);
+        @Override
+        protected Void doInBackground(String... params) {
+            String text = "";
+            BufferedReader reader=null;
+            int responseCode = 0;
+            // Send data
+            try
+            {
+                String contactDetail = "{\"lead_score\":\"44\",  \"tags\":[\"tag1\", \"tag2\"], \"properties\":[{\"type\":\"SYSTEM\", \"name\":\"email\",\"value\":\"jason1987@gmail.com\"}, {\"type\":\"SYSTEM\", \"name\":\"first_name\", \"value\":\"First_name\"}, {\"type\":\"SYSTEM\", \"name\":\"last_name\", \"value\":\"Last_name\"}]}";
 
-var contact = {
-	"id": "5764169861169152",
-    'tags': ['tag1', 'tag3'],
-    'lead_score': 20,
-    'star_value': 40,
-    'properties': [{
-        'name': 'first_name',
-        'value': 'Test',
-        'type': 'SYSTEM'
-    }]
-};
+                Log.i(TAG,"co5 = "+contactDetail);
 
-//obj.contactAPI.add(contact, success, error);
-var systemField = [{
-    'name': 'first_name',
-    'value': 'ttt'
-}];
+                // Defined URL  where to send data
+                URL url = new URL("https://ghanshyam.agilecrm.com/dev/api/contacts");
 
-var customField = [{
-    'name': 'test',
-    'value': 'ABC'
-}];
+                // Send POST data request
 
-//obj.contactAPI.update(contact, success, error);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 
-obj.contactAPI.deleteContact('5633009881448448', success, error);
+                final String basicAuth = "Basic " + Base64.encodeToString("ghanshyam.raut@agilecrm.com:123456".getBytes(), Base64.NO_WRAP);
+
+                conn.setRequestProperty ("Authorization", basicAuth);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+
+
+                OutputStream os = conn.getOutputStream();
+                os.write(contactDetail.getBytes("UTF-8"));
+                os.close();
+
+
+
+                // Get the server response
+
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                // Read Server Response
+                while((line = reader.readLine()) != null)
+                {
+                    // Append server response in string
+                    sb.append(line + "\n");
+                }
+
+
+                text = sb.toString();
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                try
+                {
+
+                    reader.close();
+                }
+
+                catch(Exception ex) {}
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+
+            if(error !=null){
+                Log.i(TAG,"Failed.......................");
+            }else{
+                Log.i(TAG,"Success.......................");
+            }
+        }
+    }
